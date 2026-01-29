c5b6d1d77681b701aa5c58cea0906996f6d35bfa
aswathyjoshy095-lab <aswathyjoshy095@gmail.com>
2026-01-29 11:34:02 +0530

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class sort 
+{
+public static void main(string args[])
+{
+int n,i,j;
+string temp;
+scanner sc=new scanner(system.in);
+scanner st=new scanner(system.in);
+system.out.println("Enter number of strings");
+n=sc.nextInt();
+string[] str=new string[n];
+system.out.println("Enter the strings: ");
+for(i=0;i<n,i++){ 
+str[i]=st.nextLine();
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(str[i].compareTo(str[j]>0)
+{
+temp=str[i];
+str[i]=str
